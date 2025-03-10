4a6073afa7ad6397a85fc977a17c52481ac2a1d4
Luan-Rafael <luan.tec18@gmail.com>
2025-03-10 18:22:30 -0300

    feat: add route rooms and create room controller

--- backend/src/controllers/rooms-controller.js
+++ backend/src/controllers/rooms-controller.js
@@ -1,40 +1,48 @@
-import { execute, findAll } from "../db/exec.js"
+import { execute, find, findAll } from "../db/exec.js"
 import { ApiError } from "../utils/api-error.js"
 
-export async function getRooms(_,response,next) {
-    try {    
-        const rooms = await findAll(`select * from rooms`)    
+export async function getRooms(_, response, next) {
+    try {
+        const rooms = await findAll(`select * from rooms`)
 
-        response.json({rooms})       
+        response.json({ rooms })
     } catch (error) {
-        next(error)        
+        next(error)
     }
 }
 
-export async function createRoom(request,response,next) {
-    const {name} = request.body
-    try {    
-        await execute(`insert into rooms (name), values (?);`, [name])
+export async function createRoom(request, response, next) {
+    const { id } = request.user
+    const { name } = request.body
+    try {
 
-        response.status(201).send()       
+        const room =  await find(`select * from rooms where name = ?`, [name])
+
+        if(room) {
+            throw new ApiError(`A sala ${name} já foi criada.`, 400)
+        }
+
+        await execute(`insert into rooms (name, user_id) values (?, ?);`, [name, id])
+
+        response.status(201).send()
     } catch (error) {
-        next(error)        
+        next(error)
     }
 }
 
-export async function updateRoom(request,response,next) {
-    const {id} = request.params
-    const {name} = request.body
+export async function updateRoom(request, response, next) {
+    const { id } = request.params
+    const { name } = request.body
 
-    try {   
-        if(!(id && name)) {
+    try {
+        if (!(id && name)) {
             throw new ApiError("Dados ausentes", 400)
         }
 
-        const rooms = await findAll(`update set name = ? from rooms where id = ?`, [name, id])    
+        const rooms = await findAll(`update set name = ? from rooms where id = ?`, [name, id])
 
-        response.json({rooms})       
+        response.json({ rooms })
     } catch (error) {
-        next(error)        
+        next(error)
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { login, registerUser } from "./controllers/users-controller.js";
 import { authHandler } from "./middlewares/auth.js";
-import { getRooms } from "./controllers/rooms-controller.js";
+import { createRoom, getRooms } from "./controllers/rooms-controller.js";
 
 const router = Router();
 
 router.get("/v1/rooms", authHandler, getRooms);
+router.post("/v1/rooms", authHandler, createRoom);
 router.post("/v1/login", login);
 router.post("/v1/signup", registerUser);
 
