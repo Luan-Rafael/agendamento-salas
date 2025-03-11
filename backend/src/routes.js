6e20224871fb109545d918eb93403224d968de92
Luan-Rafael <luan.tec18@gmail.com>
2025-03-11 00:47:17 -0300

    feat: add getUsers route and update UsersPage with email display

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,21 @@
 import { Router } from "express";
-import { login, registerUser } from "./controllers/users-controller.js";
+import {
+  getUsers,
+  login,
+  registerUser,
+} from "./controllers/users-controller.js";
 import { authHandler } from "./middlewares/auth.js";
 import { createRoom, getRooms } from "./controllers/rooms-controller.js";
 
 const router = Router();
 
-router.get("/v1/rooms", authHandler, getRooms);
-router.post("/v1/rooms", authHandler, createRoom);
 router.post("/v1/login", login);
 router.post("/v1/signup", registerUser);
+
+router.get("/v1/rooms", authHandler, getRooms);
+router.post("/v1/rooms", authHandler, createRoom);
+
+router.get("/v1/users", authHandler, getUsers);
+router.post("/v1/users");
 
 export { router };
