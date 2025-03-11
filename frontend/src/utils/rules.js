8697f224fad493ff9851c517eda50bd2230f580f
Luan-Rafael <luan.tec18@gmail.com>
2025-03-11 12:43:48 -0300

    feat: add rules

--- frontend/src/utils/rules.js
+++ frontend/src/utils/rules.js
@@ -1,8 +1,24 @@
+const THREE_HOURS_IN_MILLISSECONDS = 1000 * 60 * 60 * 3
+
 const rules = {
   userName: val => val && val.length > 6 || 'Nome obrigatório',
-  email:(value) => /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value) || "Email obrigatório",
-  required:  val => !!val || "Campo obrigatório",
-  minLenght: (val)=>val && val.length >= 6 || 'Campo inválido',
+  email: (value) => /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value) || "Email obrigatório",
+  required: val => !!val || "Campo obrigatório",
+  minLenght: (val) => val && val.length >= 6 || 'Campo inválido',
+  isDateTodayOrFuture: (value) => {
+    const date = new Date(value)
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    return new Date(date.getTime() + THREE_HOURS_IN_MILLISSECONDS) >= today || 'A data deve ser hoje ou no futuro';
+  },
+  isTimeNowOrFuture: (value) => {
+    const date = new Date()
+    const dateValue = new Date();
+    const [hour, minutes] = value.split(':')
+    dateValue.setHours(hour, minutes) 
+    return dateValue >= new Date(date.getTime()) || "A hora deve ser atual ou no futuro"
+  }
 }
 
 export default rules
