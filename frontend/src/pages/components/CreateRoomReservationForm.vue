68f631c41adafecdd2a58927fc45af6564e63daf
Luan-Rafael <luan.tec18@gmail.com>
2025-03-11 20:41:58 -0300

    feat: enhance room reservation form with description and validation improvements

--- frontend/src/pages/components/CreateRoomReservationForm.vue
+++ frontend/src/pages/components/CreateRoomReservationForm.vue
@@ -1,28 +1,57 @@
 <template>
-  {{ optionRooms }} <br>
-  {{ model }}
-  <q-dialog v-model="model.open">
-    <q-card>
+  <q-dialog v-model="model.open" persistent>
+    <q-card class="card-form">
       <q-card-section>
         <div class="text-h6">Agendar uma sala</div>
       </q-card-section>
       <q-form @submit="onSubmit">
-        <q-card-section class="q-pt-none q-pa-md" style="max-width: 300px">
-          <q-select v-model="model.room" :options="optionRooms" label="Sala" />
-          <q-input v-model="model.date" label="Data" type="date" :rules="[rules.required, rules.isDateTodayOrFuture]" />
-          <custom-input-time v-model="model.startTime" label="Hora inicio" :rules="[rules.required, rules.isTimeNowOrFuture]" />
-          <custom-input-time v-model="model.endTime" label="Hora termino" />
+        <q-card-section class="q-pt-none q-pa-md">
+          <q-select
+            v-model="model.room"
+            :options="optionRooms"
+            label="Sala"
+            :rules="[rules.required]"
+          />
+          <q-input v-model="model.description" label="Descrição" :rules="[rules.required]" />
+          <q-input
+            v-model="model.date"
+            label="Data"
+            type="date"
+            :rules="[rules.required, rules.isDateTodayOrFuture]"
+          />
+          <custom-input-time
+            v-model="model.startTime"
+            label="Hora inicio"
+            :rules="[rules.required, isToday ? rules.isTimeNowOrFuture : true]"
+          />
+          <custom-input-time
+            v-model="model.endTime"
+            label="Hora termino"
+            :rules="[rules.required, (value) => rules.isTimeFutureTo(value, model.startTime)]"
+          />
         </q-card-section>
 
         <q-card-actions class="flex q-mt-xs">
           <q-btn class="w-full" label="cancelar" @click="onReset" color="grey" v-close-popup />
           <q-space />
-          <q-btn class="w-full" color="cyan-8" label="Salvar" type="submit" :loading="model.isLoading" />
+          <q-btn
+            class="w-full"
+            color="cyan-8"
+            label="Salvar"
+            type="submit"
+            :loading="model.isLoading"
+          />
         </q-card-actions>
       </q-form>
     </q-card>
   </q-dialog>
-  <q-btn style="margin-left: auto;" @click="() => openDialog({ isOpen: true })" color="cyan-8" rounded size="md">
+  <q-btn
+    style="margin-left: auto"
+    @click="() => openDialog({ isOpen: true })"
+    color="cyan-8"
+    rounded
+    size="md"
+  >
     Novo Agendamento
     <q-icon class="q-ml-xs" name="today" />
   </q-btn>
@@ -32,34 +61,52 @@
 import { useQuasar } from 'quasar'
 import { api } from 'src/boot/axios'
 import CustomInputTime from 'src/components/CustomInputTime.vue'
-import { onMounted, reactive, ref, inject } from 'vue'
+import { onMounted, reactive, ref, inject, computed } from 'vue'
 
 const rules = inject('rules')
 
 const emit = defineEmits('refresh')
+
 const $q = useQuasar()
 const optionRooms = ref([])
+
+const isToday = computed(() => {
+  if (!model.date) {
+    return true
+  }
+  const now = new Date()
+  const date = new Date(`${model.date}T00:00:00`)
+  now.setHours(0, 0, 0, 0)
+
+  return now.getTime() == date.getTime()
+})
 
 const model = reactive({
   open: false,
   id: null,
   description: '',
   room: null,
-  date: new Date(),
+  date: null,
   startTime: null,
   endTime: null,
-  isLoading: false
+  isLoading: false,
 })
 
-function openDialog({ isOpen = false, id = null, name = '' }) {
+function openDialog({ isOpen = false }) {
   model.open = isOpen
-  model.id = id
-  model.name = name
 }
 
 async function onSubmit() {
   model.isLoading = true
-  await api.post('/v1/reservations', { name: model.name })
+
+  await api
+    .post('/v1/reservation', {
+      description: model.description,
+      roomId: model.room.value,
+      date: model.date,
+      startTime: model.startTime,
+      endTime: model.endTime,
+    })
     .then(() => {
       $q.notify({
         message: 'Registro salvo com sucesso',
@@ -67,6 +114,7 @@ async function onSubmit() {
         position: 'bottom',
         timeout: 1000,
       })
+      onReset()
     })
     .catch((error) => {
       $q.notify({
@@ -75,26 +123,32 @@ async function onSubmit() {
         position: 'bottom',
         timeout: 1000,
       })
-    }).finally(() => {
+    })
+    .finally(() => {
       emit('refresh')
-      onReset()
-
     })
 }
 
 function onReset() {
   model.open = false
   model.id = null
-  model.name = ''
+  model.description = ''
+  model.room = null
+  model.date = null
+  model.startTime = null
+  model.endTime = null
   model.isLoading = false
 }
 
 onMounted(async () => {
   const response = await api.get('/v1/rooms')
-  console.log(response)
   const { rooms } = response.data
-  optionRooms.value = rooms.map(e => ({ label: e.name, value: e.id }))
+  optionRooms.value = rooms.map((e) => ({ label: e.name, value: e.id }))
 })
-
-
 </script>
+<style scoped>
+.card-form {
+  width: 100%;
+  max-width: 500px;
+}
+</style>
