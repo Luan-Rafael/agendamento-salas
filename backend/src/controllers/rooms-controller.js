8da9c2b2796b66394c236070f1f19d6721bc9464
Luan-Rafael <luan.tec18@gmail.com>
2025-03-11 01:16:56 -0300

    feat: enhance rooms and bookings functionality with new routes and error handling

--- backend/src/controllers/rooms-controller.js
+++ backend/src/controllers/rooms-controller.js
@@ -1,48 +1,73 @@
-import { execute, find, findAll } from "../db/exec.js"
-import { ApiError } from "../utils/api-error.js"
+import { execute, find, findAll } from "../db/exec.js";
+import { ApiError } from "../utils/api-error.js";
 
 export async function getRooms(_, response, next) {
-    try {
-        const rooms = await findAll(`select * from rooms`)
+  try {
+    const rooms = await findAll(`select * from rooms`);
 
-        response.json({ rooms })
-    } catch (error) {
-        next(error)
-    }
+    response.json({ rooms });
+  } catch (error) {
+    next(error);
+  }
 }
 
 export async function createRoom(request, response, next) {
-    const { id } = request.user
-    const { name } = request.body
-    try {
+  const { id } = request.user;
+  const { name } = request.body;
+  try {
+    const room = await find(`select * from rooms where name = ?`, [name]);
 
-        const room =  await find(`select * from rooms where name = ?`, [name])
-
-        if(room) {
-            throw new ApiError(`A sala ${name} já foi criada.`, 400)
-        }
-
-        await execute(`insert into rooms (name, user_id) values (?, ?);`, [name, id])
-
-        response.status(201).send()
-    } catch (error) {
-        next(error)
+    if (room) {
+      throw new ApiError(`A sala ${name} já foi criada.`, 400);
     }
+
+    await execute(`insert into rooms (name, user_id) values (?, ?);`, [
+      name,
+      id,
+    ]);
+
+    response.status(201).send();
+  } catch (error) {
+    next(error);
+  }
 }
 
 export async function updateRoom(request, response, next) {
-    const { id } = request.params
-    const { name } = request.body
+  const { id } = request.params;
+  const { name } = request.body;
 
-    try {
-        if (!(id && name)) {
-            throw new ApiError("Dados ausentes", 400)
-        }
-
-        const rooms = await findAll(`update set name = ? from rooms where id = ?`, [name, id])
-
-        response.json({ rooms })
-    } catch (error) {
-        next(error)
+  try {
+    if (!(id && name)) {
+      throw new ApiError("Dados ausentes", 400);
     }
+
+    const rooms = await findAll(`update set name = ? from rooms where id = ?`, [
+      name,
+      id,
+    ]);
+
+    response.json({ rooms });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getBookings(request, response, next) {
+  try {
+    const { id } = request.user;
+    const bookings = await findAll(`select * from bookings`, [id]);
+    response.json({ bookings });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function createBooking(request, response, next) {
+  try {
+    const { id } = request.user;
+    const bookings = await findAll(`select * from bookings`, [id]);
+    response.json({ bookings });
+  } catch (error) {
+    next(error);
+  }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,7 +5,12 @@ import {
   registerUser,
 } from "./controllers/users-controller.js";
 import { authHandler } from "./middlewares/auth.js";
-import { createRoom, getRooms } from "./controllers/rooms-controller.js";
+import {
+  createBooking,
+  createRoom,
+  getBookings,
+  getRooms,
+} from "./controllers/rooms-controller.js";
 
 const router = Router();
 
@@ -13,9 +18,12 @@ router.post("/v1/login", login);
 router.post("/v1/signup", registerUser);
 
 router.get("/v1/rooms", authHandler, getRooms);
-router.post("/v1/rooms", authHandler, createRoom);
+router.post("/v1/room", authHandler, createRoom);
 
 router.get("/v1/users", authHandler, getUsers);
-router.post("/v1/users");
+router.post("/v1/user");
+
+router.get("/v1/bookings", authHandler, getBookings);
+router.post("/v1/booking", authHandler, createBooking);
 
 export { router };
