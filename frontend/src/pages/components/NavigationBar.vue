da8b2013e30ec328e5a79fbc4218c745c9366cd4
Luan-Rafael <luan.tec18@gmail.com>
2025-03-12 03:11:08 -0300

    feat: add NavigationBar component for calendar navigation controls

---
+++ frontend/src/pages/components/NavigationBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="row justify-center">
+    <div class="q-pa-md q-gutter-lg row">
+      <q-btn no-caps dense @click="$emit('today')"> Hoje </q-btn>
+      <q-btn no-caps dense @click="$emit('prev')"> &lt; Anterior </q-btn>
+      <q-btn no-caps dense @click="$emit('next')"> Próximo &gt; </q-btn>
+    </div>
+  </div>
+</template>
