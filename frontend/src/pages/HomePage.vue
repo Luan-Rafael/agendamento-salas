e371e2bc388497d6c06797f23e600d2b4ee0d524
Luan-Rafael <luan.tec18@gmail.com>
2025-03-11 21:45:30 -0300

    feat: add calendar selection and display to home page

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,29 +1,45 @@
 <template>
-    <div class="flex column">
-        <create-room-reservation-form @refresh="fetchReservations"/>
-        {{ rows }}
+  <div class="flex">
+    <q-select
+      v-model="selectedCalendar"
+      label="Tipo visão"
+      outlined
+      dense
+      options-dense
+      :options="['Diário', 'Mensal']"
+      style="min-width: 180px"
+    />
+
+    <create-room-reservation-form @refresh="fetchReservations" />
+    <div style="display: flex; width: 100%; max-height: calc(100vh - 9rem)">
+      <q-calendar ref="calendar"></q-calendar>
     </div>
+  </div>
 </template>
 
 <script setup>
 import CreateRoomReservationForm from './components/CreateRoomReservationForm.vue'
+import { QCalendar } from '@quasar/quasar-ui-qcalendar'
 import { useQuasar } from 'quasar'
 import { api } from 'src/boot/axios'
 import { ref } from 'vue'
 const $q = useQuasar()
 const rows = ref([])
 
+const selectedCalendar = ref('Diário')
+const calendar = ref()
+
 const fetchReservations = async () => {
-    const response = await api.get('/v1/reservations').catch((error) => {
-        $q.notify({
-            message: error.message,
-            color: 'negative',
-            position: 'bottom',
-            timeout: 1000,
-        })
+  const response = await api.get('/v1/reservations').catch((error) => {
+    $q.notify({
+      message: error.message,
+      color: 'negative',
+      position: 'bottom',
+      timeout: 1000,
     })
-    const { reservations } = response.data
-    rows.value = reservations
+  })
+  const { reservations } = response.data
+  rows.value = reservations
 }
 
 fetchReservations()
