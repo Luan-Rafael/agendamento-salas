292fc3ffe7f6e92dc10ecff518d673d536b70b7e
Luan-Rafael <luan.tec18@gmail.com>
2025-03-13 13:58:39 -0300

    cleanup: remove comment

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -43,7 +43,7 @@ export default defineBoot(({ app , router}) => {
         }
       }
 
-      return Promise.reject({message: error.response?.data?.message ?? message}); // Rejeita a promise para que o erro possa ser tratado onde a requisição for chamada
+      return Promise.reject({message: error.response?.data?.message ?? message});
     }
   );
 
