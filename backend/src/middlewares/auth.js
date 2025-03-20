48087a5b70dcbfebcddbee6112851ac6ccbb257d
Luan-Rafael <luan.tec18@gmail.com>
2025-03-20 11:52:51 -0300

    fix(auth): fix authentication middleware error

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -3,14 +3,14 @@ import { ApiError } from "../utils/api-error.js";
 import { secretKey } from "../../index.js";
 
 export const authHandler = (req, res, next) => {
-  const token = req.header("Authorization").replace("Bearer ", "");
+  const token = req.header("Authorization");
 
   if (!token) {
     throw new ApiError("No token provided", 401);
   }
 
   try {
-    const decoded = jwt.verify(token, secretKey);
+    const decoded = jwt.verify(token.replace("Bearer ", ""), secretKey);
     req.user = decoded;
     next();
   } catch (err) {
